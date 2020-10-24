fd5655492f293fbfc866286948212502acdcbafa
takeo <takeo.takeo331@gmail.com>
2020-10-24 23:47:59 +0900

    仕様ボタンのWidgetをファイルに切り分け見通しをよくした

--- lib/view/page/home.dart
+++ lib/view/page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_issues/view/widget/specification_button.dart';
 import 'package:flutter_issues/view/widget/tab_widget.dart';
 
 class Home extends StatelessWidget {
@@ -18,15 +19,7 @@ class Home extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: ElevatedButton(
-            child: const Text('仕様はここをタップ'),
-            style: ElevatedButton.styleFrom(
-                shape: const StadiumBorder(),
-                primary: Colors.lightBlueAccent,
-                onPrimary: Colors.white,
-                elevation: 5),
-            onPressed: () => _dialog(context),
-          ),
+          title: const SpecificationButton(),
           bottom: TabBar(
             tabs: _tab,
             isScrollable: true,
@@ -44,45 +37,5 @@ class Home extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void _dialog(BuildContext context) {
-    showDialog<void>(
-        context: context,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: AlertDialog(
-            title: const Text(
-              '仕様',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20),
-            ),
-            content: const SingleChildScrollView(
-              child: Text(
-                '''
-ご確認いただき、誠にありがとうございます。
-仕様について簡単に記します。
-  
-●基本機能として定められている「ラベルごとのIssueをリスト表示できる」機能の実装。
-
-●pullRefresh機能を追加。
-
-●はじめは20件読み込み、その後スクロールごとに20件ずつ読み込む。
-
-●リストは作成日時の降順で、openとclosed両方のissueを表示する様にしている。
-
-●アイコンについて、openのときは緑マーク、closeしたissueは赤いチェックとなる。
-
-●issueのカードをタップすると、webページに遷移する様にしている。''',
-                style: TextStyle(fontSize: 14),
-              ),
-            ),
-            actions: [
-              FlatButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('閉じる'))
-            ],
-          ),
-        ));
   }
 }
---
+++ lib/view/widget/specification_button.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class SpecificationButton extends StatelessWidget {
+  const SpecificationButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: const Text('仕様はここをタップ'),
+      style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          primary: Colors.lightBlueAccent,
+          onPrimary: Colors.white,
+          elevation: 5),
+      onPressed: () => _dialog(context),
+    );
+  }
+
+  void _dialog(BuildContext context) {
+    showDialog<void>(
+        context: context,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: AlertDialog(
+            title: const Text(
+              '仕様',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            ),
+            content: const SingleChildScrollView(
+              child: Text(
+                '''
+ご確認いただき、誠にありがとうございます。
+仕様について簡単に記します。
+  
+●基本機能として定められている「ラベルごとのIssueをリスト表示できる」機能の実装。
+
+●pullRefresh機能を追加。
+
+●はじめは20件読み込み、その後スクロールごとに20件ずつ読み込む。
+
+●リストは作成日時の降順で、openとclosed両方のissueを表示する様にしている。
+
+●アイコンについて、openのときは緑マーク、closeしたissueは赤いチェックとなる。
+
+●issueのカードをタップすると、webページに遷移する様にしている。
+
+''',
+                style: TextStyle(fontSize: 14),
+              ),
+            ),
+            actions: [
+              FlatButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('閉じる'))
+            ],
+          ),
+        ));
+  }
+}
